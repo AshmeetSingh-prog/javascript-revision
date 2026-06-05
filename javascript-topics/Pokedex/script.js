744e1d54541f25485e58dff8be20dd8fcf314242
Ashmeet <ashmeetsingh1445c@gmail.com>
2026-06-05 20:54:37 +0530

    changeImage() done

--- javascript-topics/Pokedex/script.js
+++ javascript-topics/Pokedex/script.js
@@ -1,3 +1,40 @@
-fetch("https://pokeapi.co/api/v2/pokemon/1/")
-  .then(res => res.json())
-  .then(data => console.log(data["name"]));
+
+async function getPokeData(name){
+      try{
+            const api = `https://pokeapi.co/api/v2/pokemon/${name}`
+            const response = await fetch(api)
+            const data = await response.json()
+            return data
+      }
+      catch(Error){
+            console.log(Error);            
+      }
+};
+const loadPokemon = async (name) => {
+      const data = await getPokeData(name)
+      changeImage(data)
+}
+async function changeImage(data){
+      const url = data['sprites']['other']['official-artwork']['front_default']
+      let img = document.getElementById("img")
+      img.src = url
+      console.log(url)
+}
+loadPokemon("pikachu")
+
+
+
+
+
+
+
+
+
+/* async function loadPokemon(name){
+      const data = await getPokeData(name);
+
+      changeImage(data);
+      changeHeight(data);
+      changeWeight(data);
+      changeName(data);
+} */
